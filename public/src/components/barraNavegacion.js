66e8864aff8e284fecd54885ac14dcf8a60de3c0
demendozaatumesa <franciscomacariomitchell@gmail.com>
2023-02-28 17:53:07 -0300

    creacion pag productos que faltaban

--- public/src/components/barraNavegacion.js
+++ public/src/components/barraNavegacion.js
@@ -16,10 +16,10 @@ class navBar extends HTMLElement {
                         <li><a class="link">Productos</a>
                             <ul class="sub-menu">
                                 <li><a href="./coccion.html" class="link prod-link" id="coccion.html">Cocción</a></li>
-                                <li><a href="./calefaccion" class="link prod-link" id="calefaccion">Calefacción</a></li>
-                                <li><a href="./aguacaliente" class="link prod-link" id="aguacaliente">Agua caliente</a></li>
-                                <li><a href="./lavadosecado" class="link prod-link" id="lavadosecado">Lavado/Secado</a></li>
-                                <li><a href="./heladerasfreezers" class="link prod-link" id="heladerasfreezers">Heladeras/Freezers</a></li>
+                                <li><a href="./calefaccion.html" class="link prod-link" id="calefaccion">Calefacción</a></li>
+                                <li><a href="./aguacaliente.html" class="link prod-link" id="aguacaliente">Agua caliente</a></li>
+                                <li><a href="./lavadosecado.html" class="link prod-link" id="lavadosecado">Lavado/Secado</a></li>
+                                <li><a href="./heladerasfreezers.html" class="link prod-link" id="heladerasfreezers">Heladeras/Freezers</a></li>
                             </ul>
                         </li>
                         <li><a href="./about.html" class="link">Nosotros</a></li>
